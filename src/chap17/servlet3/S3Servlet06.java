2cbdb0408d1dbc0e450345beea145cce3e7ab297
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-04-20 16:59:40 +0900

    Update S3Servlet06.java

--- src/chap17/servlet3/S3Servlet06.java
+++ src/chap17/servlet3/S3Servlet06.java
@@ -10,12 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class S3Servlet06
  */
-/* @WebServlet("/S3Servlet06") */
+//@WebServlet("/S3Servlet06")
 public class S3Servlet06 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	private String viewName;
        
+	private String viewName;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -33,7 +32,7 @@ public class S3Servlet06 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		request.getRequestDispatcher(viewName).forward(request, response);
 	}
 

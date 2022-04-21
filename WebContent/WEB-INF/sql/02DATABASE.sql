38ebe5b4647b72ae2b98e3ebf18f365a71df99c5
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-04-21 17:34:27 +0900

    sql, jdbc

---
+++ WebContent/WEB-INF/sql/02DATABASE.sql
@@ -0,0 +1,11 @@
+-- 다른 database 테이블 조회
+SELECT * FROM sys.sys_config;
+
+-- database 이동
+USE sys;
+SELECT * FROM sys_config;
+SELECT * FROM w3schools.Customers;
+SELECT * FROM Customers;
+
+USE w3schools;
+SELECT * FROM Customers;

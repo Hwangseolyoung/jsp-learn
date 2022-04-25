ab32d446f8f4780392173ff643f4dd78fbc79e74
seolyoung <97827365+Hwangseolyoung@users.noreply.github.com>
2022-04-25 19:45:23 +0900

    null

---
+++ WebContent/WEB-INF/sql/07NULL.sql
@@ -0,0 +1,15 @@
+USE w3schools;
+
+SELECT * FROM Employees ORDER BY EmployeeID DESC;
+
+SELECT * FROM Employees WHERE Photo = 'pic999';
+
+-- 조회시 IS NULL : NULL인 것 조회
+SELECT * FROM Employees WHERE Photo IS NULL;
+-- IS NOT NULL : NULL이 아닌 것 조회
+SELECT * FROM Employees WHERE Photo IS NOT NULL;
+
+-- 그룹함수에서 보통 제외됨
+SELECT COUNT(*) FROM Employees; -- COUNT : 레코드 수
+SELECT COUNT(LastName) FROM Employees;
+SELECT COUNT(Photo) FROM Employees;

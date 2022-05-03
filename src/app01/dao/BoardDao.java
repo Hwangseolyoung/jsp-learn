f4e20e374269369d976ac4011380d7b4cd798ad5
seolyoung0811@gmail.com <seolyoung0811@gmail.com>
2022-05-03 17:45:23 +0900

    left join

--- src/app01/dao/BoardDao.java
+++ src/app01/dao/BoardDao.java
@@ -53,7 +53,11 @@ public class BoardDao {
 		
 		List<BoardDto> list = new ArrayList<>();
 		
-		String sql = "SELECT id, title, inserted FROM Board ORDER BY id DESC";
+		String sql = "SELECT b.id, b.title, b.inserted, COUNT(r.id) numOfReply "
+				+ "FROM Board b "
+				+ "LEFT JOIN Reply r ON b.id = r.board_id "
+				+ "GROUP BY b.id "
+				+ "ORDER BY id DESC";
 
 		try (Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(sql);) {
@@ -63,6 +67,7 @@ public class BoardDao {
 				board.setId(rs.getInt(1));
 				board.setTitle(rs.getString(2));
 				board.setInserted(rs.getTimestamp(3).toLocalDateTime());
+				board.setNumOfReply(rs.getInt(4));
 				
 				list.add(board);
 			}
@@ -75,9 +80,10 @@ public class BoardDao {
 	}
 
 	public BoardDto get(Connection con, int id) {
-		String sql = "SELECT id, title, body, inserted "
-				+ "FROM Board "
-				+ "WHERE id = ? ";
+		String sql = "SELECT b.id, b.title, b.body, b.inserted, COUNT(r.id) numOfReply "
+				+ "FROM Board b "
+				+ "LEFT JOIN Reply r ON b.id = r.board_id "
+				+ "WHERE b.id = ? ";
 		
 		try (PreparedStatement pstmt = con.prepareStatement(sql);){
 			
@@ -90,6 +96,7 @@ public class BoardDao {
 					board.setTitle(rs.getString(2));
 					board.setBody(rs.getString(3));
 					board.setInserted(rs.getTimestamp(4).toLocalDateTime());
+					board.setNumOfReply(rs.getInt(5));
 					
 					return board;
 				}
